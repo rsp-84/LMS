c07ea65ae15792813e6d63006064ca1f6e232be5
rsp-84 <ryan.pflugradt@outlook.com>
2020-11-22 13:07:42 -0600

    Finished manager view and correcting YTD calculations

--- LMS.UI.MVC/Controllers/ReportsController.cs
+++ LMS.UI.MVC/Controllers/ReportsController.cs
@@ -54,13 +54,13 @@ namespace LMS.UI.MVC.Controllers
                                   where completedLesson.LessonId == completedLesson.Lesson.LessonId && userId == completedLesson.UserId
                                   select new { completedLesson.Lesson.LessonTitle, completedLesson.DateViewed }).ToList();
 
-            ViewBag.LessonCompleteCount = userLessonData.Count;
+            ViewBag.LessonCompleteCount = userLessonData.Where(x => x.DateViewed.Year == DateTime.Now.Year).Count();
 
             var userCourseData = (from completedCourse in db.CourseCompletions
                                   where completedCourse.CourseId == completedCourse.Course.CourseId && userId == completedCourse.UserId
                                   select new { completedCourse.Course.CourseName, completedCourse.Course.CourseImg, completedCourse.Course.Category, completedCourse.DateCompleted }).ToList();
 
-            ViewBag.CourseCompleteCount = userCourseData.Count;
+            ViewBag.CourseCompleteCount = userCourseData.Where(x => x.DateCompleted.Year == DateTime.Now.Year).Count();
 
             //Build ViewModel----------
             foreach (var item in userLessonData)
@@ -97,14 +97,50 @@ namespace LMS.UI.MVC.Controllers
             var loggedInManager = User.Identity.GetUserId();
 
             //get data
+            List<ManagerReportIndexViewModel> managerReportIndexVM = new List<ManagerReportIndexViewModel>();
+
             var allAssignedEmployees = (from employees in db.UserDetails
                                        where employees.ReportsTo == loggedInManager
-                                       select new ManagerReportIndexViewModel { EmployeeFirstName = employees.FirstName, EmployeeLastName = employees.LastName, numCoursesCompletedYTD = employees.CourseCompletions.Count, numLessonsCompletedYTD = employees.LessonViews.Count }).ToList();
+                                       select new { employees.UserId, employees.FirstName, employees.LastName }).ToList();
+            
+            //Build VM
+            foreach (var item in allAssignedEmployees)
+            {
+                ManagerReportIndexViewModel objMrVM = new ManagerReportIndexViewModel();
+                var numCourses = db.CourseCompletions.Where(x => x.UserId == item.UserId).Select(x => x.DateCompleted.Year);
+                var numLessons = db.LessonViews.Where(x => x.UserId == item.UserId).Select(x => x.DateViewed.Year);
+
+                int courseCount = 0;
+                int lessonCount = 0;
+
+                foreach (var courseYear in numCourses)
+                {
+                    if (courseYear == DateTime.Now.Year)
+                    {
+                        courseCount++;
+                    }
+                }
+                foreach (var lessonYear in numLessons)
+                {
+                    if (lessonYear == DateTime.Now.Year)
+                    {
+                        lessonCount++;
+                    }
+                }
+
+
+                objMrVM.EmployeeFirstName = item.FirstName;
+                objMrVM.EmployeeLastName = item.LastName;
+                objMrVM.numCoursesCompletedYTD = courseCount;
+                objMrVM.numLessonsCompletedYTD = lessonCount;
+
+                managerReportIndexVM.Add(objMrVM);
+            }
 
             ViewBag.ManagerImg = db.UserDetails.Find(loggedInManager).UserPhoto;
             ViewBag.ManagerName = $"{db.UserDetails.Find(loggedInManager).FirstName} {db.UserDetails.Find(loggedInManager).LastName}";
 
-            return View(allAssignedEmployees);
+            return View(managerReportIndexVM);
         }
 
         protected override void Dispose(bool disposing)
