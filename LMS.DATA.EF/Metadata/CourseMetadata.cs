ec71d4c8b94d1f51e111cdf27f4852e4ca51731b
rsp-84 <ryan.pflugradt@outlook.com>
2020-11-26 10:48:38 -0600

    Course Metadata custom propety for slug

--- LMS.DATA.EF/Metadata/CourseMetadata.cs
+++ LMS.DATA.EF/Metadata/CourseMetadata.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace LMS.DATA.EF
@@ -32,5 +33,27 @@ namespace LMS.DATA.EF
     }
 
     [MetadataType(typeof(CourseMetadata))]
-    public partial class Course { }
+    public partial class Course
+    {
+        public string GenerateSlug()
+        {
+            string phrase = string.Format("{0}-{1}", CourseId, CourseName);
+
+            string str = RemoveAccent(phrase).ToLower();
+            // invalid chars           
+            str = Regex.Replace(str, @"[^a-z0-9\s-]", "");
+            // convert multiple spaces into one space   
+            str = Regex.Replace(str, @"\s+", " ").Trim();
+            // cut and trim 
+            str = str.Substring(0, str.Length <= 45 ? str.Length : 45).Trim();
+            str = Regex.Replace(str, @"\s", "-"); // hyphens   
+            return str;
+        }
+
+        private string RemoveAccent(string text)
+        {
+            byte[] bytes = Encoding.GetEncoding("Cyrillic").GetBytes(text);
+            return Encoding.ASCII.GetString(bytes);
+        }
+    }
 }
