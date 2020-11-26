cf2e43da6a5f3517e8d987a70721007d893e03f4
rsp-84 <ryan.pflugradt@outlook.com>
2020-11-26 11:50:08 -0600

    manager slugs

--- LMS.UI.MVC/Models/ManagerReportIndexViewModel.cs
+++ LMS.UI.MVC/Models/ManagerReportIndexViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace LMS.UI.MVC.Models
@@ -22,6 +24,29 @@ namespace LMS.UI.MVC.Models
         public string CourseName { get; set; }
         public string CourseCat { get; set; }
         public DateTime CourseCompletedDate { get; set; }
+
+        public string GenerateSlug()
+        {
+            string phrase = string.Format("{0}-{1}", CourseId, CourseName);
+
+            string str = RemoveAccent(phrase).ToLower();
+            // invalid chars           
+            str = Regex.Replace(str, @"[^a-z0-9\s-]", "");
+            // convert multiple spaces into one space   
+            str = Regex.Replace(str, @"\s+", " ").Trim();
+            // cut and trim 
+            str = str.Substring(0, str.Length <= 45 ? str.Length : 45).Trim();
+            str = Regex.Replace(str, @"\s", "-"); // hyphens   
+            return str;
+
+        }
+
+        private string RemoveAccent(string text)
+        {
+            byte[] bytes = Encoding.GetEncoding("Cyrillic").GetBytes(text);
+            return Encoding.ASCII.GetString(bytes);
+        }
+
     }
 
     public class LessonsCompleted
@@ -29,5 +54,28 @@ namespace LMS.UI.MVC.Models
         public int LessonId { get; set; }
         public string LessonName { get; set; }
         public DateTime LessonCompletedDate { get; set; }
+
+        public string GenerateSlug()
+        {
+            string phrase = string.Format("{0}-{1}", LessonId, LessonName);
+
+            string str = RemoveAccent(phrase).ToLower();
+            // invalid chars           
+            str = Regex.Replace(str, @"[^a-z0-9\s-]", "");
+            // convert multiple spaces into one space   
+            str = Regex.Replace(str, @"\s+", " ").Trim();
+            // cut and trim 
+            str = str.Substring(0, str.Length <= 45 ? str.Length : 45).Trim();
+            str = Regex.Replace(str, @"\s", "-"); // hyphens   
+            return str;
+
+        }
+
+        private string RemoveAccent(string text)
+        {
+            byte[] bytes = Encoding.GetEncoding("Cyrillic").GetBytes(text);
+            return Encoding.ASCII.GetString(bytes);
+        }
     }
+
 }
